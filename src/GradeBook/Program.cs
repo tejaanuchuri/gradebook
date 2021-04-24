bb3229cc89bf9f8f6963daa662d353d271e7d5d8
tejaanuchuri <tejaanuchuri0009@gmail.com>
2021-04-24 18:11:10 +0530

    "read input via command line arguments"

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -6,7 +6,7 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Teja...!");
+            Console.WriteLine($"Hello {args[0]}...!");
         }
     }
 }
